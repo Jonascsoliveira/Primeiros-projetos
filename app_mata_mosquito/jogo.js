86922fc643300fc5070c88d98025595fa84006f3
jonas <52612513+Jonascsoliveira@users.noreply.github.com>
2019-07-21 20:49:54 -0300

    one more commit

--- app_mata_mosquito/jogo.js
+++ app_mata_mosquito/jogo.js
@@ -1,1 +1,10 @@
-alert('teste')
+var altura = 0;
+var largura = 0;
+
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    console.log(altura, largura);
+}
+ 
+
