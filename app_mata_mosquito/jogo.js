27f0a27ac96505ac6ea41eae5ac1e67c65f69e0d
jonas <52612513+Jonascsoliveira@users.noreply.github.com>
2019-07-22 23:24:44 -0300

    last commit today

--- app_mata_mosquito/jogo.js
+++ app_mata_mosquito/jogo.js
@@ -26,13 +26,40 @@ function posicaoRandomica(){
     //criando elemento HTML
     var mosquito = document.createElement('img');
     mosquito.src = 'imagens/mosquito.png';
-    mosquito.className = 'mosquito1';
+    mosquito.className = tamanhoAleatorio()+''+ladoAleatorio();
     //posicionando o eixo da imagem left-top(canto superior esquerdo).
     mosquito.style.left = posicaoX + 'px';
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.position = 'absolute';
+    //mosquito.id = 'mosquito';
 
     document.body.appendChild(mosquito);
 
+}
+
+function tamanhoAleatorio (){
+    var classe = Math.floor(Math.random() * 3);
+    
+    switch(classe){
+        case 0:
+            return 'mosquito1';
+        
+        case 1:
+            return 'mosquito2';
+
+        case 2:
+            return 'mosquito3';
+    }
+}
+
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random()*2);
+
+    switch(classe){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
 }
 
